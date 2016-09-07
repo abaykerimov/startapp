e40e4cc77335ad3fe692db52e49cb2e67f1f4491
abaykerimov <abay-001@mail.ru>
2016-09-07 13:27:29 +0600

    edit Project model fields

--- main/serializers.py
+++ main/serializers.py
@@ -6,4 +6,4 @@ from django.contrib.auth.models import User
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ('id', 'project_name', 'description', 'team', 'contacts')
+        fields = ('id', 'project_name', 'description', 'created_date', 'mission', 'location', 'url', 'date_public', 'team', 'contacts', 'category', 'status')
--- main/views.py
+++ main/views.py
@@ -1,62 +1,14 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from main.models import Project
 from main.serializers import ProjectSerializer
+from rest_framework import generics
 
 
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
+class ProjectList(generics.ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
 
 
-@csrf_exempt
-def project_list(request):
-    """
-    List all code snippets, or create a new snippet.
-    """
-    if request.method == 'GET':
-        snippets = Project.objects.all()
-        serializer = ProjectSerializer(snippets, many=True)
-        return JSONResponse(serializer.data)
+class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ProjectSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
-
-
-@csrf_exempt
-def project_detail(request, pk):
-    """
-    Retrieve, update or delete a code snippet.
-    """
-    try:
-        project = Project.objects.get(pk=pk)
-    except Project.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = ProjectSerializer(project)
-        return JSONResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = ProjectSerializer(project, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        project.delete()
-        return HttpResponse(status=204)
